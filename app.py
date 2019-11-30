22370225404d1a8e8c7cb486a78e61041eca5328
Allan Elsberry <allan.elsberry@gmail.com>
2019-11-30 11:16:23 -0500

    added Day class and calendar structure

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from config import Config
 from datetime import date, timedelta
 from calendar import monthrange
 from flask_migrate import Migrate
-
+import holidays
 
 
 
@@ -23,7 +23,7 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
-full_calendar = [date(2019,1,1) + timedelta(days=n) for n in range(365*15)]
+us_holidays = holidays.US()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,6 +40,39 @@ class User(db.Model):
     def __repr__(self):
         return f"{self.first} {self.last}"
 
+class Day():
+    def __init__(self,id,date):
+        self.id=id
+        self.date=date
+        self.set_value()
+        self.assigned=None
+        self.bid = {}
+    def get_value(self,value=None):
+        if self.date.weekday()>4:
+            return 3
+        if self.date in us_holidays:
+            return 3
+        return 2
+    def set_value(self,value=None):
+        if value:
+            self.value=value 
+        else:
+            self.value=self.get_value()
+    def insert_bid(self,user_id,bid=1):
+        self.bid.update({user_id:bid})
+        return None
+    def assign_day(self,user_id):
+        self.assigned=user_id
+        return None
+    def __repr__(self):
+        return f"{self.date.strftime('%d %b %y')}"
+
+
+
+start_date = date(2019,1,1)
+full_calendar = [Day(n,start_date + timedelta(days=n)) for n in range(365*15)]
+days_of_week = "Mon, Tue, Wed, Thu, Fri, Sat, Sun".split(',')
+
 
 def get_user():
     return User.query.get(1)
@@ -51,15 +84,16 @@ def seed_users():
     u = User(first="Allan",last="Elsberry",email="allan.elsberry@gmail.com",phone=2059015853,qualified=True)
     create_user(u)
     return None
-
-def get_calendar(day=date.today()):
-    calendar = {}
-    days_of_week = "Mon, Tue, Wed, Thu, Fri, Sat, Sun".split(',')
-    start_date = day= day-timedelta(days=(day.day-1)) #get beginning of month
-    date_list = [start_date+timedelta(days=n) for n in range(monthrange(start_date.year,start_date.month)[1])]
-    calendar.update({'date_list':date_list})
-    calendar.update({'days_of_week':days_of_week})
-    return calendar
+def get_date_index(my_date):
+    result = my_date-start_date
+    return result.days
+def get_month_calendar(my_date=date.today()):
+    first_dom = my_date-timedelta(days=my_date.day-1)
+    last_dom = first_dom+timedelta(days=monthrange(first_dom.year,first_dom.month)[1])
+    start_index = get_date_index(first_dom-timedelta(days=first_dom.weekday()))
+    end_index = get_date_index(last_dom)
+    month_calendar = full_calendar[start_index:end_index]
+    return month_calendar
     
 #seed_users()
 
@@ -78,4 +112,4 @@ def login():
 
 @app.route('/calendar')
 def calendar():
-    return render_template('calendar.html', user=get_user(), calendar = get_calendar())
+    return render_template('calendar.html', user=get_user(), calendar = get_month_calendar(),days_of_week=days_of_week)
